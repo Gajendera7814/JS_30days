abace1cab528defb0408ad9809c4dd6921b679cf
Gajendera kumar <93085197+Gajendera7814@users.noreply.github.com>
2025-10-09 22:30:42 +0530

    React Questions

---
+++ React_Questions/index.js
@@ -0,0 +1,8 @@
+
+/*
+    🚀 1. Difference between Real DOM and Virtual DOM.
+    🚀 2. Difference between Controlled and Uncontrolled Components.
+    🚀 3. What is Redux Middleware?
+    🚀 4. What is JSX in React?
+    🚀 5. Explain the working of Redux and describe its key parts/components.
+*/
